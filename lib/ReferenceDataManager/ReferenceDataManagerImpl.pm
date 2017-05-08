7bfa0aac65b22307b60a5736512d85642c62d5e5
Qizhi Zhang <qzhang@qzhang.mcs.anl.gov>
2017-05-08 09:09:25 -0500

    commented out more info printing lines due to the mongoDB oplog complaints

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -693,10 +693,10 @@ sub _indexGenomeFeatureData
         my $gn_id = $ws_gn->{name};
         #check if the genome is already present in the database by querying SOLR
         if(($solrer->exists_in_solr({search_core=>$gn_solr_core,search_query=>{genome_id=>$gn_id}}))==1) {
-            print $gn_id . ": has already been indexed in Solr " . $gn_solr_core . ".\n";
+            #print $gn_id . ": has already been indexed in Solr " . $gn_solr_core . ".\n";
         }
         else {
-            print $gn_id . ": is not found in Solr " . $gn_solr_core . ".\n";         
+            #print $gn_id . ": is not found in Solr " . $gn_solr_core . ".\n";         
             #print "\nStart to fetch the object(s) for " . $gn_id .  " on " . scalar localtime . "\n";
             eval {#return a reference to a list where each element is a Workspace.ObjectData with a key named 'data'
                 $ws_gnout = $self->util_ws_client()->get_objects2({
@@ -767,7 +767,7 @@ sub _indexGenomeFeatureData
                                 }
                             }
                             else {
-                                print "\nIndexed " . @{$gnft_batch} . " genome_feature(s) on " . scalar localtime . "\n";
+                                #print "\nIndexed " . @{$gnft_batch} . " genome_feature(s) on " . scalar localtime . "\n";
                             }
                             $gnft_batch = [];
                         }
@@ -786,7 +786,7 @@ sub _indexGenomeFeatureData
                             }
                         }
                         else {
-                            print "\nIndexed " . @{$gnft_batch} . " genome_feature(s) on " . scalar localtime . "\n";
+                            #print "\nIndexed " . @{$gnft_batch} . " genome_feature(s) on " . scalar localtime . "\n";
                         }
                         $gnft_batch = [];
                     }
